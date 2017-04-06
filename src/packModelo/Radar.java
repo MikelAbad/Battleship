fb5a7e6f954cb88a23af35d885db29ce16da5512
mendi <mendihyk@hotmail.com>
2017-04-06 14:33:38 +0200

    Clase radar

--- src/packModelo/Radar.java
+++ src/packModelo/Radar.java
@@ -1,21 +1,37 @@
 package packModelo;
 
+import java.util.ArrayList;
+
 import packModelo.packCoordenada.Coordenada;
 
 public class Radar {
 	private Coordenada posicion;
 	private int usos;
 
-	public Radar(Coordenada pC) {
-		this.posicion = pC;
-		usos = 2;
-	}
-	
-	public void mover(Coordenada pC) {
-		this.posicion = pC;
+	public Radar() {
+		this.posicion = null;
+		usos = DatosJuego.USOS_RADAR;
 	}
 
-	public void escanear() {
-		//TODO OtroSprint
+	public void mover(Coordenada pC) {
+		this.posicion = pC;
+		// TODO: Notificar al observer y eso.
+	}
+
+	public ArrayList<Coordenada> escanear() {
+		usos--;
+		return posicion.getAdyacentes();
+	}
+
+	public Coordenada getPosicion() {
+		return posicion;
+	}
+
+	public boolean usarRadar() {
+		boolean usado = false;
+		if (usos >= 1 && posicion != null) {
+			usado = true;
+		}
+		return usado;
 	}
 }
